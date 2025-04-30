e5983bc7fb9bab6ad0e55676184cfb82736c5f56
geldshot <geldshot@gmail.com>
2025-04-30 11:35:07 -0700

    rotation and soft exit added

--- main.py
+++ main.py
@@ -11,12 +11,20 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    keys = list()
+    run = True
 
-    while True:
+    while run:
         screen.fill("black")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        keys = pygame.key.get_pressed() 
+        if keys[pygame.K_ESCAPE]:
+            run = False
+
+        player.update(dt)    
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) /1000                            
--- player.py
+++ player.py
@@ -1,11 +1,24 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
 import pygame
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(dt)
+
+        if keys[pygame.K_d]:
+            self.rotate(-dt)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+    
 
     # in the player class
     def triangle(self):
